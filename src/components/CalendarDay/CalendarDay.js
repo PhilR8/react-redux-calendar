b0ce3a068fe95954d2a74bd9b11a9bba344050a0
Phil Reese <philr8@gmail.com>
2019-03-30 11:32:14 -0400

    Add mouseover/mouseout events and styling to calendarDays

--- src/components/CalendarDay/CalendarDay.js
+++ src/components/CalendarDay/CalendarDay.js
@@ -9,7 +9,8 @@ import dateFns from 'date-fns';
 const styles = theme => ( {
     dayCell: {
         flex: '1 0 13%',
-        border: '1px solid lightgray'
+        border: '1px solid lightgray',
+        cursor: 'pointer'
     },
     dayCellOutsideMonth: {
         flex: '1 0 13%',
@@ -17,9 +18,9 @@ const styles = theme => ( {
         backgroundColor: 'rgba( 211, 211, 211, 0.4 )'
     },
     dateNumber: {
-        margin: 10,
-        height: '23px',
-        width: '23px',
+        margin: 5,
+        height: '28px',
+        width: '28px',
         fontSize: '0.85rem',
         color: '#000',
         backgroundColor: 'transparent'
@@ -30,30 +31,67 @@ const styles = theme => ( {
         width: '28px',
         fontSize: '0.85rem',
         color: '#fff',
-        backgroundColor: deepPurple[500],
-    }
+        backgroundColor: deepPurple[400],
+    },
+    focusedAvatar: {
+        margin: 5,
+        height: '28px',
+        width: '28px',
+        fontSize: '0.85rem',
+        color: '#000',
+        backgroundColor: '#f1f1f1',
+    },
+    focusedTodayAvatar: {
+        margin: 5,
+        height: '28px',
+        width: '28px',
+        fontSize: '0.85rem',
+        color: '#fff',
+        backgroundColor: deepPurple[800],
+    },
     
 } );
 
 class CalendarDay extends Component {
     constructor( props ) {
         super( props );
+
+        this.state = {
+            focused: false
+        }
+    }
+
+    onMouseOver = () => {
+        this.setState( { focused: true } );
+    }
+
+    onMouseOut = () => {
+        this.setState( { focused: false } );
     }
 
     render() {
         const { classes, dateObj, calendarDate } = this.props;
+        const { focused } = this.state;
+        const isToday = dateFns.isToday( dateObj.date );
+
+        const avatarClass = isToday && focused
+            ? classes.focusedTodayAvatar
+            : isToday
+                ? classes.todayAvatar
+                : focused
+                    ? classes.focusedAvatar
+                    : classes.dateNumber;
         return (
             <div 
+                onMouseOver={ this.onMouseOver }
+                onMouseOut={ this.onMouseOut }
                 className={ 
                     dateFns.isSameMonth( dateObj.date, calendarDate ) 
                         ? classes.dayCell 
                         : classes.dayCellOutsideMonth 
                 }
             >
-                { dateFns.isToday( dateObj.date ) 
-                    ? <Avatar className={ classes.todayAvatar }>{ dateFns.getDate( dateObj.date ) }</Avatar>
-                    : <Avatar className={ classes.dateNumber }>{ dateFns.getDate( dateObj.date ) }</Avatar>
-                }
+                <Avatar className={ avatarClass }>{ dateFns.getDate( dateObj.date ) }</Avatar>
             </div>
         )
     }
