9bd30fc86a388a26b93bb50abfd1dfaaa02bb877
Phil Reese <philr8@gmail.com>
2019-03-31 14:18:54 -0400

    Revert previous commit and delete the correct thing this time

--- src/components/AddReminder/AddReminder.js
+++ src/components/AddReminder/AddReminder.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import AddReminderForm from './AddReminderForm';
-
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -23,8 +21,11 @@ import { withStyles } from '@material-ui/core/styles';
 import dateFns from 'date-fns';
 
 const styles = theme => ( {
-    remindersContainer: {
-        minHeight: '250px'
+    addReminderFormContainer: {
+        minHeight: '250px',
+        marginTop: '10px',
+        display: 'flex',
+        flexDirection: 'column'
     },
     fabAdd: {
         margin: '20px',
@@ -64,11 +65,20 @@ class AddReminder extends React.Component {
                     </IconButton>
                 </DialogTitle>
                 <Divider light />
-                <DialogContent className={ classes.remindersContainer }>
-                    <DialogContentText className={ classes.noRemindersText }>
-                        Testing
-                    </DialogContentText>
-                    <AddReminderForm />
+                <DialogContent className={ classes.addReminderFormContainer }>
+                    <TextField
+                        id="outlined-full-width"
+                        label="Add Reminder (max 30 characters)"
+                        style={{ margin: 8 }}
+                        placeholder="Example: Buy Groceries"
+                        fullWidth
+                        margin="normal"
+                        maxLength="30"
+                        variant="outlined"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button color="primary">
