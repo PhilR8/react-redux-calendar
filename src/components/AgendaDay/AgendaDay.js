62ffeee559bb463ddb35d7c39f49247f1c68332f
Phil Reese <philr8@gmail.com>
2019-03-31 22:50:52 -0400

    Add logic for delete button visibility in agenda day reminder

--- src/components/AgendaDay/AgendaDay.js
+++ src/components/AgendaDay/AgendaDay.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import CloseIcon from '@material-ui/icons/Close';
+import DeleteIcon from '@material-ui/icons/Delete';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
@@ -20,7 +21,14 @@ const styles = theme => ( {
     },
     reminder: {
         margin: '15px',
-        padding: '15px'
+        padding: '15px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    reminderText: {
+        display: 'flex',
+        alignItems: 'center'
     },
     noRemindersText: {
         marginTop: '10px'
@@ -29,8 +37,67 @@ const styles = theme => ( {
         position: 'absolute',
         right: '10px',
         top: '10px'
+    }, 
+    deleteButtonHidden: {
+        visibility: 'hidden'  
+    },
+    deleteButtonVisible: {
+        visibility: 'visible'
     }
 } );
+
+class Reminder extends React.Component {
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+            reminderFocused: false
+        }
+    }
+
+    onReminderMouseOver = () => {
+        this.setState( { reminderFocused: true } );
+    }
+    
+    onReminderMouseOut = () => {
+        this.setState( { reminderFocused: false } );
+    }
+     
+    render() {
+        const { classes, reminder } = this.props;
+        const deleteBtnClass = this.state.reminderFocused
+            ? classes.deleteButtonVisible
+            : classes.deleteButtonHidden;
+            
+        return (
+            <Paper 
+                className={ classes.reminder } 
+                elevation={ 1 }
+                style={{ backgroundColor: reminder.color }}
+                onMouseOver={ this.onReminderMouseOver }
+                onMouseOut={ this.onReminderMouseOut }
+            >
+                <Typography variant='h5' className={ classes.reminderText }>
+                    { dateFns.format( reminder.time, 'H:mma' ) }: { reminder.text }
+                </Typography>
+                <IconButton className={ deleteBtnClass } aria-label="Delete">
+                    <DeleteIcon />
+                </IconButton>
+            </Paper>
+            
+        ) 
+    }
+}
+
+Reminder.propTypes = {
+    classes: PropTypes.object.isRequired,
+    reminder: PropTypes.shape( {
+        date: PropTypes.instanceOf( Date ),
+        time: PropTypes.instanceOf( Date ),
+        color: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired
+    } )
+}
 
 class AgendaDay extends React.Component {
     render() {
@@ -60,16 +127,7 @@ class AgendaDay extends React.Component {
                                 No reminders found for this date.
                               </DialogContentText>
                             : reminders.map( ( reminder, i ) => 
-                                <Paper 
-                                    key={ i } 
-                                    className={ classes.reminder } 
-                                    elevation={ 1 }
-                                    style={{ backgroundColor: reminder.color }}
-                                >
-                                    <Typography variant='h5'>
-                                        { dateFns.format( reminder.time, 'H:mma' ) }: { reminder.text }
-                                    </Typography>
-                                </Paper>
+                                <Reminder key={ i } classes={ classes } reminder={ reminder } />
                             ) 
                     }
                 </DialogContent>
